794b06b79a4fe6cfca46a8f4369ae8a086eb1a37
B-ryce001 <89813842+B-ryce001@users.noreply.github.com>
2021-11-03 11:20:21 -0400

    Made interface class

---
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/GerbilsInterface.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spitefulgerbilsapp;
+
+/**
+ *
+ * @author jones
+ */
+public interface GerbilsInterface 
+{
+    public void setCharInfo();
+    public void accessCharInfo();
+    
+    
+    public void setNewChar();
+    public void getNewChar();
+    
+    public void setUserAccount();
+    public void getUserAcoount();
+}

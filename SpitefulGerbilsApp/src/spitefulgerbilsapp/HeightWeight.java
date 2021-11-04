915e92c05309e97bf1f0aec7f31a5dcc98d0006c
CmrnBnsn <89947267+CmrnBnsn@users.noreply.github.com>
2021-11-04 14:55:53 -0400

    Update HeightWeight.java

--- SpitefulGerbilsApp/src/spitefulgerbilsapp/HeightWeight.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/HeightWeight.java
@@ -6,43 +6,52 @@ import java.util.Scanner;
 public class HeightWeight {
 
 // public static void main(String[] args) { - NetBeans says you don't need this because this isn't a tester class.
-    public void answers() {
+
+    public void answers(){
         Scanner gender = new Scanner(System.in);
         System.out.println("Are you male or female");
-        String mf = gender.nextLine();
+        mf = gender.nextLine();
+        if(mf == "male"){
+            mf = "male";
+        }else{
+            mf = "female";
+        }
     }
     
-    public void humanHeight(String mf){
+    
+    public void humanHeight(){
         Random rand = new Random();
         if(mf == "male"){
             int max = 89;
             int min = 54;
-            double randomNum = rand.nextDouble(max + 1 - min) + min;
-            modHeight = randomNum%12;
-            maleHeight = randomNum/12;
-            System.out.println("Your Height is " + maleHeight + "'" + modHeight);
-        }else{
+            int randomNum = rand.nextInt(max - min) + min;
+            int modHeight = randomNum%12;
+            int maleHeight = randomNum/12;
+            System.out.println("Your Height is " + maleHeight + "'" + modHeight + '"' + ".");
+        }else if(mf == "female"){
             int max = 79;
             int min = 52;
-            double randomNum = rand.nextDouble(max + 1 - min) + min;
-            modHeight = randomNum%12;
-            femaleHeight = randomNum/12;
-            System.out.println("Your Height is " + femaleHeight + "'" + modHeight);
+            int randomNum = rand.nextInt(max - min) + min;
+            int modHeight = randomNum%12;
+            int femaleHeight = randomNum/12;
+            System.out.println("Your Height is " + femaleHeight + "'" + modHeight + '"' + ".");
+        
         }
     }
-    public void humanWeight(String mf){
+    public void humanWeight(){
+        Random rand = new Random();
             if(mf == "male"){
             int max = 260;
             int min = 90;
-            double randomNumTwo = rand.nextDouble(max + 1 - min) + min;
-            System.out.println("Your Weight is " + randomNumTwo + " lbs");
-        }else{
+            int randomNumTwo = rand.nextInt(max - min) + min;
+            System.out.println("Your Weight is " + randomNumTwo + " lbs.");
+        }else if(mf == "female"){
             int max = 200;
             int min = 75;
-            double randomNumTwo = rand.nextDouble(max + 1 - min) + min;
-            System.out.println("Your Weight is " + randomNumTwo + " lbs");
+            int randomNumTwo = rand.nextInt(max - min) + min;
+            System.out.println("Your Weight is " + randomNumTwo + " lbs.");
+        
         }
     }
-}
-//}
 
+}

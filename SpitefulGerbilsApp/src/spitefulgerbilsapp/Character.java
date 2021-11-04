7bd74afea40f9cdebd2e193cfbf07130701aec5e
Jariel Jimenez-Lopez <36284359+jarieljimenez@users.noreply.github.com>
2021-11-04 15:46:26 -0400

    Update Character.java

--- SpitefulGerbilsApp/src/spitefulgerbilsapp/Character.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/Character.java
@@ -1,12 +1,21 @@
 package spitefulgerbilsapp;
-
+import java.util.Scanner;
 /**
  *
  * @author XWP921
  */
 public class Character implements GerbilsInterface {
     public void setCharInfo() {
-        
+        // Instantiate the variables for the character.
+        String name;
+        String gender;
+        // Create an object for the Scanner class
+        Scanner in = new Scanner(System.in);
+        // Prompt the user for a name for their characer, then save that to name.
+        System.out.print("Enter a name for your character: ");
+        name = in.nextLine();
+        System.out.print("Select a gender - (M) Male or (F) Female: ");
+        gender = in.nextLine();
     }
     public void accessCharInfo() {
         

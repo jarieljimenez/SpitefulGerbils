3465ada17bd73fe222de7d855e03e70d65843fae
B-ryce001 <89813842+B-ryce001@users.noreply.github.com>
2021-11-05 11:38:36 -0400

    Update DiceRoller.java

--- SpitefulGerbilsApp/src/spitefulgerbilsapp/DiceRoller.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/DiceRoller.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spitefulgerbilsapp;
 
 /**
  *
  * @author jones
  */
-public class DiceRoller {
-    
+import java.util.Random;
+public class DiceRoller
+{
+    public static  int roll3d6Dice(int number, int nSides)
+    { 
+        int num = 0;
+        int roll = 0;
+        Random  r = new Random(); 
+        if(nSides >=3) 
+        { 
+            for(int i = 0; i < number; i++)
+            { 
+                roll = r.nextInt(nSides)+1;
+                System.out.println("Roll is:  "+roll);
+                num = num + roll; 
+            } 
+        } 
+        else
+        { 
+            System.out.println("Error num needs to be from 3"); 
+        } 
+        return num;  
+    } 
+     public static  int roll5d6Dice(int number, int nSides)
+         { 
+        int num = 0;
+        int roll = 0;
+        Random  r = new Random(); 
+        if(nSides >=5) 
+        { 
+            for(int i = 0; i < number; i++)
+            { 
+                roll = r.nextInt(nSides)+1;
+                System.out.println("Roll is:  "+roll);
+                num = num + roll; 
+            } 
+        } 
+        else
+        { 
+            System.out.println("Error num needs to be from 5"); 
+        } 
+        return num;  
+    }        
+             
+    public static void main(String[] args)
+    {
+        System.out.println("Total is: "+roll3d6Dice(3, 6)+ "\n");
+        System.out.println("Total is: "+roll5d6Dice(5, 6));
+    }
 }
+

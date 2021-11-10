2e4cd8ab47a7fe6e2d03ee6b44222fe71b81c99a
JacobHoback <kxx426@mocs.utc.edu>
2021-11-10 16:45:41 -0500

    Adding Jacobs Code
    Added title screen and inserted main code and also added files

--- SpitefulGerbilsApp/src/User Accounts/src/user/accounts/TitleScreen.java
+++ SpitefulGerbilsApp/src/User Accounts/src/user/accounts/TitleScreen.java
@@ -1,19 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package spitefulgerbilsapp;
+
+
+
+package user.accounts;
 
 /**
  *
  * @author jacob
  */
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
+//import java.util.ArrayList; // import the ArrayList class
 
 public class TitleScreen 
 {
--- SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
@@ -1,5 +1,12 @@
 package spitefulgerbilsapp;
 
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Scanner;
+import static user.accounts.TitleScreen.signIn;
+import static user.accounts.TitleScreen.signUp;
 /**
  *
  * @author XWP921
@@ -11,13 +18,36 @@ public class SpitefulGerbilsApp {
      */
     public static void main(String[] args) 
     {
-    DiceRoller dice = new DiceRoller();
+            DiceRoller dice = new DiceRoller();
     
     
-    //dice.roll3d6Dice(3,6);
-    dice.roll5d6Dice(5,6);
+            //dice.roll3d6Dice(3,6);
+            dice.roll5d6Dice(5,6);
     
+<<<<<<< HEAD
+    
+        TitleScreen test1 = new TitleScreen(); //Jacobs shit
         
+        //test1.menuSelection();
+        //test1.createAccount();
+=======
+    //dice.roll3d6Dice(3,6);
+    //dice.roll5d6Dice(5,6);
+     HeightWeight yuh = new HeightWeight();
+     yuh.run();
+>>>>>>> 9cf165ab83a6c5782929586cecbee440d46072f9
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to sign up for user account, 2 to log in ");
+        int decision = scanner.nextInt();
+        if (decision == 1) 
+        {
+            signUp();
+        } 
+        else if (decision == 2) 
+        {
+            signIn();
+        }
     }
     
 }

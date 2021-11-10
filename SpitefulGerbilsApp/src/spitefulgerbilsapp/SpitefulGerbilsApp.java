6f85919cb6edd2287ae1745045666466281c33b7
JacobHoback <kxx426@mocs.utc.edu>
2021-11-10 17:03:25 -0500

    Update SpitefulGerbilsApp.java

--- SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
@@ -16,26 +16,25 @@ public class SpitefulGerbilsApp {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException 
     {
             DiceRoller dice = new DiceRoller();
     
     
             //dice.roll3d6Dice(3,6);
             dice.roll5d6Dice(5,6);
+            //dice.roll3d6Dice(3,6);
+            //dice.roll5d6Dice(5,6);
+
     
-<<<<<<< HEAD
-    
+            
+            
         TitleScreen test1 = new TitleScreen(); //Jacobs shit
         
         //test1.menuSelection();
         //test1.createAccount();
-=======
-    //dice.roll3d6Dice(3,6);
-    //dice.roll5d6Dice(5,6);
-     HeightWeight yuh = new HeightWeight();
-     yuh.run();
->>>>>>> 9cf165ab83a6c5782929586cecbee440d46072f9
+
+    
         
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 to sign up for user account, 2 to log in ");

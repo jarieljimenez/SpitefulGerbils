fb8c910b6f6f10dcda95d53fd4aee7a07657407e
Jaylan-Oliver <ojaylan4@gmail.com>
2021-11-10 17:38:45 -0500

    was testing heightweight with main class

--- SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
@@ -11,7 +11,7 @@ public class SpitefulGerbilsApp {
      */
     public static void main(String[] args) 
     {
-<<<<<<< HEAD
+
             DiceRoller dice = new DiceRoller();
     
     
@@ -28,17 +28,6 @@ public class SpitefulGerbilsApp {
         //test1.menuSelection();
         //test1.createAccount();
 
-=======
-    DiceRoller dice = new DiceRoller();
     
->>>>>>> f43310275152b3311b30f21f641c03f3d0117367
-    
-    //dice.roll3d6Dice(3,6);
-    //dice.roll5d6Dice(5,6);
-    //dice.rollExtra5d6Dice(5,6);
-     //HeightWeight yuh = new HeightWeight();
-     //yuh.run();
-        
-    }
-    
+}
 }

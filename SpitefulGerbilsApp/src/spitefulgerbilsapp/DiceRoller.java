0092b672acc1e56b1d3600fffb20a6f8e449fdca
B-ryce001 <89813842+B-ryce001@users.noreply.github.com>
2021-11-10 17:00:50 -0500

    Dice Roller Fixed

--- SpitefulGerbilsApp/src/spitefulgerbilsapp/DiceRoller.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/DiceRoller.java
@@ -6,36 +6,36 @@ package spitefulgerbilsapp;
  */
 import java.util.Random;
 public class DiceRoller
-{
-    
+{   
     public static int roll1d3Dice(int number, int nSides)
     {
+        int result = 0;
         int num = 0;
         int roll = 0;
         Random  r = new Random(); 
-        if(nSides >=3) 
+        if(nSides >=1) 
         { 
-            for(int i = 0; i < number; i++)
+            for(int i = 0;i > 1 || i < number; i++)
             { 
                 roll = r.nextInt(nSides)+1;
                 System.out.println("Roll is:  "+roll);
-                num = num + roll; 
+                result = num + roll; 
             } 
         } 
         else
         { 
             System.out.println("Error num needs to be from 3"); 
         } 
-        return num; 
+        return result; 
     }
     public static int roll3d6Dice(int number, int nSides)
     { 
         int num = 0;
         int roll = 0;
         Random  r = new Random(); 
-        if(nSides >=3) 
+        if(number >=3) 
         { 
-            for(int i = 0; i < number; i++)
+            for(int i = 0;i > 3 || i < number; i++)
             { 
                 roll = r.nextInt(nSides)+1;
                 System.out.println("Roll is:  " + roll);
@@ -46,21 +46,21 @@ public class DiceRoller
         { 
             System.out.println("Error num needs to be from 3"); 
         } 
-        System.out.print("Total is: " + roll3d6Dice(number, nSides)+ "\n");
+        System.out.println("Total is: " + num+ "\n");
         return num;  
     } 
     
      public static int roll5d6Dice(int number, int nSides)
-         { 
+        { 
         int num = 0;
         int roll = 0;
         Random  r = new Random(); 
-        if(nSides >=5) 
+        if(number >= 5) 
         { 
-            for(int i = 0; i < number; i++)
+            for(int i = 0; i > 5 || i < number; i++)
             { 
                 roll = r.nextInt(nSides)+1;
-                System.out.println("Roll is:  "+roll);
+                System.out.println("Roll is:  " + roll);
                 num = num + roll; 
             } 
         } 
@@ -68,19 +68,21 @@ public class DiceRoller
         { 
             System.out.println("Error num needs to be from 5"); 
         } 
-        System.out.println("Total is: "+roll5d6Dice(number,nSides) + "\n");
+        System.out.println("Total is: "+ num + "\n");
         return num;  
     }        
              
     public static int rollExtra5d6Dice(int number,int nSides)
      {
+        int result = 0;
         int sum = 0;
         int num = 0;
         int roll = 0;
+        result = roll1d3Dice(1,3);
         Random  r = new Random(); 
-        if(nSides >=3) 
+        if(nSides >=5) 
         { 
-            for(int i = 0; i < number; i++)
+            for(int i = 0;i > 5 || i < number; i++)
             { 
                 roll = r.nextInt(nSides)+1;
                 System.out.println("Roll is:  "+ roll);
@@ -91,7 +93,7 @@ public class DiceRoller
         { 
             System.out.println("Error num needs to be from 5"); 
         } 
-        System.out.println("Total is: "+ sum + roll1d3Dice(1,3));
+        System.out.println("Total is: "+ sum + result);
         return sum; 
      }
 }

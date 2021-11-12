7e19d0c35fc02887174f8005fee1a1de6a2fc2a3
B-ryce001 <89813842+B-ryce001@users.noreply.github.com>
2021-11-12 11:43:38 -0500

    Update SpitefulGerbilsApp.java

--- SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
@@ -12,15 +12,14 @@ public class SpitefulGerbilsApp {
     public static void main(String[] args) 
     {
 
-            //DiceRoller dice = new DiceRoller();
+            DiceRoller dice = new DiceRoller();
     
     
-            //dice.roll3d6Dice(3,6);
-            //dice.roll5d6Dice(5,6);
-            //dice.roll3d6Dice(3,6);
-            //dice.roll5d6Dice(5,6);
+            dice.roll3d6Dice(3,6);
+            dice.roll5d6Dice(5,6);
+            dice.rollExtra5d6Dice(5,6);
             HeightWeight yuh = new HeightWeight();
-            yuh.run();
+            //yuh.run();
             
             
         TitleScreen test1 = new TitleScreen(); //Jacobs shit

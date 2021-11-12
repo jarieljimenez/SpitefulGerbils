4139e5dace6bb118660a7b5e8f58e0b8409d167a
Jariel Jimenez-Lopez <36284359+jarieljimenez@users.noreply.github.com>
2021-11-12 11:46:18 -0500

    Fixed Formatting on Tester Class

--- SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
@@ -9,25 +9,21 @@ public class SpitefulGerbilsApp {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
 
-            //DiceRoller dice = new DiceRoller();
-    
-    
-            //dice.roll3d6Dice(3,6);
-            //dice.roll5d6Dice(5,6);
-            //dice.roll3d6Dice(3,6);
-            //dice.roll5d6Dice(5,6);
-            HeightWeight yuh = new HeightWeight();
-            yuh.run();
-            
-            
-        TitleScreen test1 = new TitleScreen(); //Jacobs shit
-        
-        //test1.menuSelection();
-        //test1.createAccount();
+        // DiceRoller dice = new DiceRoller();
 
-    
-}
+        // dice.roll3d6Dice(3,6);
+        // dice.roll5d6Dice(5,6);
+        // dice.roll3d6Dice(3,6);
+        // dice.roll5d6Dice(5,6);
+        HeightWeight yuh = new HeightWeight();
+        yuh.run();
+
+        TitleScreen test1 = new TitleScreen(); // Jacobs shit
+
+        // test1.menuSelection();
+        // test1.createAccount();
+
+    }
 }

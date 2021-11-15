c689408f06610b4767294a511faab7caa8e787d3
B-ryce001 <89813842+B-ryce001@users.noreply.github.com>
2021-11-15 01:59:30 -0500

    yes main class

--- SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
@@ -13,16 +13,21 @@ public class SpitefulGerbilsApp {
     {
 
             DiceRoller dice = new DiceRoller();
+
     
     
-            dice.roll3d6Dice(3,6);
-            dice.roll5d6Dice(5,6);
-            dice.rollExtra5d6Dice(5,6);
-            HeightWeight yuh = new HeightWeight();
+            //dice.roll3d6Dice(3,6);
+            //dice.roll3d6Dice(3,6);
+            //dice.roll5d6Dice(5,6);
+            //dice.rollExtra5d6Dice(5,6);
+            //dice.chooseDice();
+                    
+            HeightWeight test = new HeightWeight();
             //yuh.run();
+            //test.generateRaceScore();
             
             
-        TitleScreen test1 = new TitleScreen(); //Jacobs shit
+        //TitleScreen test1 = new TitleScreen(); //Jacobs shit
         
         //test1.menuSelection();
         //test1.createAccount();

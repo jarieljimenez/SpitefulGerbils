544d207e1e12ffd76ba4783af8f73b1b2105fa74
CmrnBnsn <89947267+CmrnBnsn@users.noreply.github.com>
2021-11-15 21:21:18 -0500

    Update SpitefulGerbilsApp.java

--- SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
+++ SpitefulGerbilsApp/src/spitefulgerbilsapp/SpitefulGerbilsApp.java
@@ -13,21 +13,9 @@ public class SpitefulGerbilsApp {
      */
     public static void main(String[] args) throws IOException {
 
-        // DiceRoller dice = new DiceRoller();
-
-        // dice.roll3d6Dice(3,6);
-        // dice.roll3d6Dice(3,6);
-        // dice.roll5d6Dice(5,6);
-        // dice.rollExtra5d6Dice(5,6);
-        // dice.chooseDice();
-
-        HeightWeight test = new HeightWeight();
-        // yuh.run();
-        // test.generateRaceScore();
-        test.charGen();
-
-        // TitleScreen test1 = new TitleScreen(); //Tester for Title Screen Methods
-        // test1.firstmenu();
+    
+            HeightWeight test = new HeightWeight();
+            test.charGen();
 
     }
 }
